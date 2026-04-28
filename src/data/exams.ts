5994d2728dac0db45cd1fbbec9469f33489b9b03
TEEJAN TEEPEE <teejan107@gmail.com>
2026-04-28 07:15:18 +0530

    Update exam schedule dates

--- src/data/exams.ts
+++ src/data/exams.ts
@@ -1,8 +1,8 @@
 import type { Exam } from '../types';
 
 export const EXAMS: Exam[] = [
-  { code: 'CST302', name: 'Compiler Design', date: '2026-04-24', credit: 4 },
-  { code: 'CST304', name: 'CGIP', date: '2026-04-28', credit: 4 },
+  { code: 'CST302', name: 'Compiler Design', date: '2026-05-14', credit: 4 },
+  { code: 'CST304', name: 'CGIP', date: '2026-04-29', credit: 4 },
   { code: 'CST306', name: 'AAD', date: '2026-05-02', credit: 4 },
   { code: 'CST372', name: 'Data & Computer Communication', date: '2026-05-05', credit: 3 },
   { code: 'HUT300', name: 'IEFT', date: '2026-05-08', credit: 3 },
